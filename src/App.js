b0b9bc9d330fabc7c4d3acfb59ca787a30a3ddd8
vladimir-jovchev <vladimir.jovchev@students.finki.ukim.mk>
2021-03-28 20:45:36 +0200

    add [components, api]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
 
+import { Cards,Chart,CountryPicker } from "./components";
+import styles from './App.modules.css';
+import { fetchData } from './api';
+
 class App extends React.Component{
+    state = {
+        data: {},
+    }
+    
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+        this.setState({data:fetchedData})
+        //console.log(fetchedData);
+     }
+    
     render(){
+        const {data} = this.state
         return(
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                
+                <Cards data={data}/>
+                <CountryPicker />
+                <Chart />
+                
             </div>
         )
     }
