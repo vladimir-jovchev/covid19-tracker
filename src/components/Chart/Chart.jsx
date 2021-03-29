ab3d5c7fea1edce3e466fe37cc25ee275ae8dcea
vladimir-jovchev <vladimir.jovchev@students.finki.ukim.mk>
2021-03-29 14:46:08 +0200

    lineChart finished,fetchData finished ...

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
+
 
 const Chart = () => {
+    const[dailyData, setDailyData] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+                //const dailyData = await fetchDailyData;
+        }
+
+        fetchAPI();
+    });
+
+    const lineChart = (
+        dailyData.length ? (
+        <Line 
+        data={{
+                labels: dailyData.map(({ date }) => date),
+                datasets: [{
+                    data: dailyData.map(({ confirmed }) => confirmed),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill: false,
+                },{
+                    data: dailyData.map(({ deaths }) => deaths),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                    fill: false,
+                }],
+            }}
+        />): null
+    ); 
+
     return (
-        <h1>Chart</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     )
 }
 
